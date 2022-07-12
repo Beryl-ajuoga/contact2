87be7dcf0575d0fd77a8dac548bcfc8dcb28999e
beryl-ajuoga <berrylajuoga@gmail.com>
2022-07-12 19:35:00 +0300

    first commit

--- app/src/main/java/dev/beryl/contactreader/DataRvAdapter.kt
+++ app/src/main/java/dev/beryl/contactreader/DataRvAdapter.kt
@@ -6,36 +6,44 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.view.menu.MenuView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import dev.beryl.contactreader.databinding.ContactListItemBinding
 
 class DataRvAdapter( var contactList: List<Datainformation>):
-    RecyclerView.Adapter<dataViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): dataViewHolder {
-        var itemView=LayoutInflater.from(parent.context)
-            .inflate(R.layout.contact_list_item,parent,false)
 
-        return dataViewHolder(itemView)
+    RecyclerView.Adapter<DataViewHolder>() {
+    override fun onCreateViewHolder(parent:ViewGroup, viewType: Int):DataViewHolder {
+        var binding = ContactListItemBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return DataViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: dataViewHolder, position: Int) {
-        var currentContact=contactList.get(position)
-        holder.tvName.text=currentContact.name
-        holder.tvEmail.text=currentContact.email
-        holder.tvAddress.text=currentContact.address
-        holder.tvPhonenumber.text=currentContact.phoneNumber
+    override fun onBindViewHolder(holder: DataViewHolder, position: Int) {
+        with(holder.binding){
+            var currentContact = contactList.get(position)
+            tvName.text = currentContact.name
+            tvEmail.text = currentContact.email
+            tvAddress.text = currentContact.address
+            tvPhonenumber.text = currentContact.phoneNumber
 
+            Picasso.get()
+                .load(currentContact.image)
+                .resize(300 ,300)
+                .centerCrop()
+                .placeholder(R.drawable.ic_baseline_person_24)
+                .error(R.drawable.ic_baseline_person_24)
+                .into(holder.binding.imgPerson)
+        }
 
     }
-
     override fun getItemCount(): Int {
         return contactList.size
 
     }
 
 }
-class dataViewHolder( itemView:View):RecyclerView.ViewHolder(itemView){
-    var tvName:TextView=itemView.findViewById(R.id.tvName)
-    var tvPhonenumber:TextView=itemView.findViewById(R.id.tvPhonenumber)
-    var tvEmail:TextView=itemView.findViewById(R.id.tvEmail)
-    var tvAddress:TextView=itemView.findViewById(R.id.tvAddress)
+class DataViewHolder(var binding: ContactListItemBinding)
+    :RecyclerView.ViewHolder(binding.root){
 
 }
--- app/src/main/java/dev/beryl/contactreader/MainActivity.kt
+++ app/src/main/java/dev/beryl/contactreader/MainActivity.kt
@@ -16,9 +16,9 @@ class MainActivity : AppCompatActivity() {
 
     }
     fun displayContacts(){
-        var contact1=Datainformation("lulu Doe","+2542380044","luludoe@gmail.com","", "616 korongo")
-        var contact2=Datainformation("ken Oloo","+256130046327" ,"olooken@gmail.com","","c18 hilly")
-        var contact3=Datainformation("sabdio wanda","+254784000053","sabdiow@gmail.com","","200 waters")
+        var contact1=Datainformation("lulu Doe","+2542380044","luludoe@gmail.com","https://images.unsplash.com/photo-1583994009785-37ec30bf9342?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGJsYWNrJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60", "616 korongo" )
+        var contact2=Datainformation("ken Oloo","+256130046327" ,"olooken@gmail.com","https://images.unsplash.com/photo-1583994009785-37ec30bf9342?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGJsYWNrJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60","c18 hilly")
+        var contact3=Datainformation("sabdio wanda","+254784000053","sabdiow@gmail.com","https://images.unsplash.com/photo-1583994009785-37ec30bf9342?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGJsYWNrJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60","200 waters")
         var contactsList= listOf(contact1,contact2,contact3)
         var contactsAdapter=DataRvAdapter(contactsList)
         binding.rvContacts.layoutManager=LinearLayoutManager(this)
